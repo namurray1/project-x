8fe14b229b1e9892a0a56ecd00a5578f25ac3ad6
Alex Murray <namurray1@gmail.com>
2018-10-15 20:56:13 -0400

    make changes to app.js file. Almost done with add setInterval to code

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,45 @@
-let words = ['Barber', 'Stylist', 'Yoga Studio', ]
-let element = document.body;
-let currectWord = -1;
+// let words = ['Barber', 'Stylist', 'Yoga Studio', ]
+// let timer = setInterval(myWords, 5000);
 
-window.setInterval(function(){
-    currentWord++;
-    if(currentWord > words.length) currentWord = 0;
-    element.textContent = words[currentWord];
-}, 5000);
+// fuction myWords() {
+//     for (let i = 0; i < words.length; i++) {
+//         document.getElementById("change-word").innerHTML = words
+//     }
+// }
+// let element = document.body;
+// let currectWord = -1;
+
+// window.setInterval(function(){
+//     currentWord++;
+//     if(currentWord > words.length) currentWord = 0;
+//     element.textContent = words[currentWord];
+// }, 5000);
+
+// test this out using a for loop
+//     for (let i = 0; i < words.length -1; i++ ) {
+        
+//     }
+
+let words = ["Barber", "Hairdresser", "Stylist", "Yoga Studio", "Massage Therapy", "MakeUp"];
+// let currentWordIndex = -1;
+
+// let wordInt = setInterval(function() {
+//     ++currentWordIndex;
+//     if (currentWordIndex > words.length) {
+//         currentWordIndex = 0;
+//     }
+//     setSpanWord(words[currentWordIndex]);
+// }, 5000); 
+
+// document.getElementById("change-word").innerHTML = words;
+
+for (let i = 0; i < words.length; ++i) {
+    setDelay(i);
+}
+
+function setDelay(i) {
+    setTimeout(function(){
+        document.getElementById("change-word").innerHTML = words;
+        // console.log(i);
+    }, 5000)
+}
