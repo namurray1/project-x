5c9845a2698816f45d6bbaeb07f20444f1572fa5
Alex Murray <namurray1@gmail.com>
2018-10-30 22:10:56 -0400

    add some files to css and js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
-let words = ["Barber", "Hairdresser", "Stylist", "Yoga Studio", "Massage Therapy", "MakeUp"];
-let wordChange = document.getElementById(change-word);
+// You need to set up a setinterval method to loop through an array here to change the name of the service
 
-function myFunction(words) {
-    setInterval(function(){ document.getElementById("change-word").innerHTML = words; }, 5000);
-}
+// let words = ["Barber", "Hairdresser", "Stylist", "Yoga Studio", "Massage Therapy", "MakeUp"];
+// let wordChange = document.getElementById(change-word);
+
+// function myFunction(words) {
+//     setInterval(function(){ document.getElementById("change-word").innerHTML = words; }, 5000);
+// }
+
